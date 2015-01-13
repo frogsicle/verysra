aca12883d9caef3fe04a015141b1396a99437cbe
Alisandra Denton <adenton@conrath.bio3.rwth-aachen.de>
2015-01-13 14:34:33 +0100

    comments on NCBI queries that might help

---
+++ get_metasra.py
@@ -0,0 +1,8 @@
+__author__ = 'adenton'
+
+
+#download meta data from SRA
+#e.g.
+# wget -O  'http://trace.ncbi.nlm.nih.gov/Traces/sra/sra.cgi?save=efetch&db=sra&rettype=runinfo&term='
+# http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=sra&term=txid39367&retmax=30
+#http://eutils.ncbi.nlm.nih.gov/entrez/eutils/elink.fcgi?db=taxonomy&dbfrom=taxonomy&id=4143
